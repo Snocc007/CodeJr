9ee65495597e874d298099962597aeb3f1984584
Snocc <kellmegegyfiok@hotmail.com>
2020-07-17 20:31:09 +0200

    Draw square is done surprisingly quick

---
+++ Practice/Draw Square/cmake-build-debug/CMakeFiles/Draw_Square.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Draw_Square.dir/main.cpp.o"
+  "Draw_Square"
+  "Draw_Square.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Draw_Square.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Practice/Draw Square/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    int a;
+    std::cout << "Give me a number (integer): " << std::endl;
+    std::cin >> a;
+    while (a < 3) {
+        std::cout << "The number must be higher." << std::endl;
+        std::cout << "Give me another number (integer): " << std::endl;
+        std::cin >> a;
+    }
+    for (int i = 1; i <= a; ++i) {
+        if (i == 1 or i == a) {
+            for (int j = 0; j < a; ++j) {
+                std::cout << "%";
+            }
+        } else {
+            std::cout << "%";
+            for (int j = 1; j < a; ++j) {
+                std::cout << " ";
+            }
+        }
+            std::cout << "%";
+
+        std::cout << std::endl;
+
+    }
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
