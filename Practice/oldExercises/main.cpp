f8cb554a77e1e890e75d2a17a68cebb70e6ebf65
Snocc <kellmegegyfiok@hotmail.com>
2020-10-16 16:04:39 +0200

    Matchmaking done

--- Practice/oldExercises/main.cpp
+++ Practice/oldExercises/main.cpp
@@ -4,6 +4,8 @@
 
 std::vector<std::string> putSaturn(std::vector<std::string> listToCorrect);
 
+std::vector<std::string> makingMatches(const std::vector<std::string> &girls, const std::vector<std::string> &boys);
+
 int main()
 {
     // Solar system
@@ -17,11 +19,26 @@ int main()
     // Expected output: Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune
     std::vector<std::string> correctPlanets = putSaturn(planets);
 
-    for (int i = 0; i < correctPlanets.size() - 1; ++i) {
+    for (int i = 0; i < correctPlanets.size(); ++i) {
         std::cout << correctPlanets[i] << " ";
     }
 
     std::cout << std::endl;
+
+    //Match Making
+
+    std::vector<std::string> girls = {"Eve", "Ashley", "Claire", "Kat", "Jane"};
+    std::vector<std::string> boys = {"Joe", "Fred", "Tom", "Todd", "Neef", "Jeff"};
+
+    // Write a method that joins the two lists by matching one girl with one boy into a new list
+    // If someone has no pair, he/she should be the element of the list too
+    // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+    std::vector<std::string> matches = makingMatches(girls, boys);
+
+    for (int i = 0; i < matches.size(); ++i) {
+        std::cout << matches[i] << " ";
+    }
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
@@ -30,7 +47,7 @@ int main()
 std::vector<std::string> putSaturn(std::vector<std::string> listToCorrect)
 {
     std::vector<std::string>::iterator itVec;
-    for (int i = 0; i < listToCorrect.size() - 1; ++i) {
+    for (int i = 0; i < listToCorrect.size(); ++i) {
         if (listToCorrect[i] == "Uranus") {
             itVec = listToCorrect.begin() + i;
         }
@@ -38,4 +55,27 @@ std::vector<std::string> putSaturn(std::vector<std::string> listToCorrect)
     listToCorrect.insert(itVec, "Saturn");
 
     return listToCorrect;
+}
+
+std::vector<std::string> makingMatches(const std::vector<std::string> &girls, const std::vector<std::string> &boys)
+{
+    std::vector<std::string> result;
+
+    if (boys.size() > girls.size()) {
+        for (int i = 0; i < boys.size(); i++) {
+            if (i < girls.size()) {
+                result.push_back(girls[i]);
+            }
+            result.push_back(boys[i]);
+        }
+    } else if (boys.size() < girls.size()) {
+        for (int i = 0; i < girls.size(); i++) {
+            if (i < boys.size()) {
+                result.push_back(boys[i]);
+            }
+            result.push_back(girls[i]);
+        }
+    }
+
+    return result;
 }
