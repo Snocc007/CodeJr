f6beb81909ba913dab28f5a882a345cf46350b1b
Snocc <kellmegegyfiok@hotmail.com>
2020-10-17 16:17:03 +0200

    Student counter done

--- Practice/oldExercises/main.cpp
+++ Practice/oldExercises/main.cpp
@@ -17,6 +17,11 @@ bool checkNums(const std::vector<int> input, const std::vector<int> checker);
 
 std::vector<std::string> quoteSwap(std::vector<std::string> &input);
 
+std::vector<std::string>
+getChildrenWithMoreThanFourCandies(const std::vector<std::pair<std::string, std::map<std::string, int>>> &students);
+
+int sumOfAgeWithLessThanFiveCandies(const std::vector<std::pair<std::string, std::map<std::string, int>>> &students);
+
 int main()
 {
     // SOLAR SYSTEM
@@ -177,6 +182,57 @@ int main()
 
     std::cout << out << std::endl;
 
+    // STUDENT COUNTER
+
+    std::vector<std::pair<std::string, std::map<std::string, int>>> students;
+
+    std::map<std::string, int> TheodorData;
+    TheodorData.insert(std::make_pair("age", 9));
+    TheodorData.insert(std::make_pair("candies", 2));
+    students.push_back(std::make_pair("Theodor", TheodorData));
+
+    std::map<std::string, int> PaulData;
+    PaulData.insert(std::make_pair("age", 10));
+    PaulData.insert(std::make_pair("candies", 1));
+    students.push_back(std::make_pair("Paul", PaulData));
+
+    std::map<std::string, int> MarkData;
+    MarkData.insert(std::make_pair("age", 7));
+    MarkData.insert(std::make_pair("candies", 3));
+    students.push_back(std::make_pair("Mark", MarkData));
+
+    std::map<std::string, int> PeterData;
+    PeterData.insert(std::make_pair("age", 12));
+    PeterData.insert(std::make_pair("candies", 5));
+    students.push_back(std::make_pair("Peter", PeterData));
+
+    std::map<std::string, int> OlafData;
+    OlafData.insert(std::make_pair("age", 12));
+    OlafData.insert(std::make_pair("candies", 7));
+    students.push_back(std::make_pair("Olaf", OlafData));
+
+    std::map<std::string, int> GeorgeData;
+    GeorgeData.insert(std::make_pair("age", 3));
+    GeorgeData.insert(std::make_pair("candies", 2));
+    students.push_back(std::make_pair("George", GeorgeData));
+
+    // Display the following things:
+    //  - The names of students who have more than 4 candies
+    //  - The sum of the age of children who have less than 5 candies
+
+    std::cout << "Children with more than 4 candies: ";
+
+    std::vector<std::string> childrenWithManyCandies = getChildrenWithMoreThanFourCandies(students);
+
+    for (int i = 0; i < childrenWithManyCandies.size(); ++i) {
+        std::cout << childrenWithManyCandies[i] << " ";
+    }
+
+    std::cout << std::endl;
+    std::cout << "Sum of those who have less than 5 candies: " << sumOfAgeWithLessThanFiveCandies(students)
+              << std::endl;
+
+
     return 0;
 }
 
@@ -302,4 +358,36 @@ std::vector<std::string> quoteSwap(std::vector<std::string> &input)
     }
 
     return input;
+}
+
+std::vector<std::string> getChildrenWithMoreThanFourCandies
+        (const std::vector<std::pair<std::string, std::map<std::string, int>>> &students)
+{
+    std::vector<std::string> result;
+
+    for (int i = 0; i < students.size(); i++) {
+        for (auto itStudents = students[i].second.begin(); itStudents != students[i].second.end(); itStudents++) {
+            itStudents = students[i].second.find("candies");
+            if (itStudents->second > 4) {
+                result.push_back(students[i].first);
+            }
+        }
+    }
+    return result;
+}
+
+int sumOfAgeWithLessThanFiveCandies(const std::vector<std::pair<std::string, std::map<std::string, int>>> &students)
+{
+    int result = 0;
+
+    for (int i = 0; i < students.size(); i++) {
+        for (auto itStudents = students[i].second.begin(); itStudents != students[i].second.end(); itStudents++) {
+            itStudents = students[i].second.find("candies");
+            if (itStudents->second < 5) {
+                auto itStudents2 = students[i].second.find("age");
+                result += itStudents2->second;
+            }
+        }
+    }
+    return result;
 }
