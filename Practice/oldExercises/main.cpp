5da8ef2c7f4c573493ea82a67832e456998952d4
Snocc <kellmegegyfiok@hotmail.com>
2020-10-17 15:04:29 +0200

    He will never done

--- Practice/oldExercises/main.cpp
+++ Practice/oldExercises/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <map>
 
 std::vector<std::string> putSaturn(std::vector<std::string> listToCorrect);
 
@@ -145,6 +146,36 @@ int main()
         }
     }
 
+    std::cout << std::endl;
+
+    // HE WILL NEVER
+
+    std::string out = "";
+    std::vector<int> notSoCrypticMessage = {1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11};
+
+    std::map<int, std::string> map;
+
+    map[7] = "run around and desert you";
+    map[50] = "tell a lie and hurt you ";
+    map[49] = "make you cry, ";
+    map[2] = "let you down";
+    map[12] = "give you up, ";
+    map[1] = "Never gonna ";
+    map[11] = "\n";
+    map[3] = "say goodbye ";
+
+    // Things are a little bit messed up
+    // Your job is to decode the notSoCrypticMessage by using the map as a look up table
+    // Assemble the fragments into the out variable
+
+    std::map<int, std::string>::iterator itMap;
+
+    for (int i = 0; i < notSoCrypticMessage.size(); ++i) {
+        itMap = map.find(notSoCrypticMessage[i]);
+        out += itMap->second;
+    }
+
+    std::cout << out << std::endl;
 
     return 0;
 }
