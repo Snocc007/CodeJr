a34c8c2c3195b2bf25e2843c2e9b7aa1c76e0605
Snocc <kellmegegyfiok@hotmail.com>
2020-10-22 19:43:37 +0200

    Print each line done

--- Practice/FileManipulations/main.cpp
+++ Practice/FileManipulations/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 void divide(double number);
+
+void printEachLine();
 
 int main()
 {
     divide(0);
 
     std::cout << "Hello, World!" << std::endl;
+
+    printEachLine();
+
     return 0;
 }
 
@@ -21,4 +28,26 @@ void divide(double number)
     } catch (...) {
         std::cout << "Division with 0 is not possible!" << std::endl;
     }
+}
+
+void printEachLine()
+{
+    std::ifstream myFile;
+
+    try {
+        myFile.open("my-file.txt");
+        if (!myFile.is_open()) {
+            throw 0;
+        }
+        std::string text;
+
+        while (myFile) {
+            getline(myFile, text);
+            std::cout << text << std::endl;
+        }
+    } catch (int x) {
+        std::cout << "Couldn't open file." << std::endl;
+    }
+
+    myFile.close();
 }
